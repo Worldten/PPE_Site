88710492361f3dc6c3f7a09cc27065fe73d03836
Thomas ZIMMERMANN <thomaszim@free.fr>
2019-06-02 23:04:47 +0200

    Add contrats

--- Contrats.js
+++ Contrats.js
@@ -1,17 +1,20 @@
 $.getJSON("https://api.thomaszimmermann.fr/locations", function (object) {
     $.each(object.data, function (index, value) {
-        let contrat = value.date_contrat_loc;
-        let entree = value.date_entree_loc;
-        let sortie = value.date_sortie_loc;
+        let c = value.date_contrat_loc;
+        let contrat = c.substring(0, c.indexOf('T'));
+        let e = value.date_entree_loc;
+        let entree = e.substring(0, e.indexOf('T'));
+        let s = value.date_sortie_loc;
+        let sortie = s.substring(0, s.indexOf('T'));
+        console.log(sortie);
         let paie = value.etat_paiement_frais_loc;
         let bien = value.reference_bien;
-        console.log(bien);
         $.getJSON(`https://api.thomaszimmermann.fr/bien?id=` + bien, function (object) {
             $.each(object.data, function (index, value) {
                 let b_prix = value.prix_bien;
                 let b_charge = value.charge_bien;
                 let b_lien = "Bien.html?id=" + bien;
-                $('.contrats').append('<div class="bien"><div class="name titre"> Date du contrat : ' + contrat + '</div><div class="name titre"> Etat du paiement : ' + paie + '</div><div class="adresse titre">Entrée du locataire' + entree + '</div><div class="prix titre">Sortie du locataire' + sortie + '</div><div class="adresse titre">Loyer mensuel : ' + b_prix + '€</div><div class="adresse titre">Charges mensuels : ' + b_charge + '€</div><a href="' + b_lien + '" class="lien_bien">Voir le bien</a></div>');
+                $('.contrats').append('<div class="bien"><div class="name titre"> Date du contrat : ' + contrat + '</div><div class="name titre"> Etat du paiement : ' + paie + '</div><div class="adresse titre">Entrée du locataire : ' + entree + '</div><div class="prix titre">Sortie du locataire : ' + sortie + '</div><div class="adresse titre">Loyer mensuel : ' + b_prix + '€</div><div class="adresse titre">Charges mensuels : ' + b_charge + '€</div><a href="' + b_lien + '" class="lien_bien">Voir le bien</a></div>');
 
             })
         })
